63f7ce8f4c643290524ea9ff176013ed229c550c
Chris Ottersen <christopherottersen@gmail.com>
2015-06-07 16:45:08 -0700

    checkin

---
+++ NewSilentAuction/src/backend/AuctionTest.java
@@ -0,0 +1,58 @@
+package backend;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AuctionTest
+{
+  
+  @Test
+  public void testAuction()
+  {
+    fail("Not yet implemented");
+  }
+  
+  @Test
+  public void testAddBidder()
+  {
+    fail("Not yet implemented");
+  }
+  
+  @Test
+  public void testAddDonor()
+  {
+    fail("Not yet implemented");
+  }
+  
+  @Test
+  public void testAddItem()
+  {
+    fail("Not yet implemented");
+  }
+  
+  @Test
+  public void testAddCash()
+  {
+    fail("Not yet implemented");
+  }
+  
+  @Test
+  public void testWriteItem()
+  {
+    fail("Not yet implemented");
+  }
+  
+  @Test
+  public void testWriteBidder()
+  {
+    fail("Not yet implemented");
+  }
+  
+  @Test
+  public void testGeneralStats()
+  {
+    fail("Not yet implemented");
+  }
+  
+}
---
+++ NewSilentAuction/src/backend/Bidder.java
@@ -0,0 +1,67 @@
+package backend;
+
+/**
+ * This class creates a new bidder.
+ * 
+ * @author The Expendables
+ * @version 0.0.0.1
+ * @since 20.05.2015
+ */
+public class Bidder extends User
+{
+
+//Constructors
+
+	/**
+	 * Creates a new bidder.
+	 * 
+	 * @param name is the name of the bidder.
+	 * @param email is some contact info for the bidder.
+	 * @param phone more contact info.
+	 */
+	public Bidder(String name, String email, String phone)
+	{
+		super(name, email, phone);
+	}
+
+	/**
+	 * Creates a new bidder.
+	 * 
+	 * @param name is the name of the bidder.
+	 * @param email is some contact info for the bidder.
+	 * @param phone more contact info.
+	 * @param ID is the id for this bidder.
+	 */
+	public Bidder(String name, String email, String phone, int ID)
+	{
+		super(name, email, phone, ID);
+	}
+
+//Methods
+
+	/**
+	 * This method allows a bidder to place a bid on an item.
+	 * 
+	 * @param item the item to be bidded on.
+	 * @param amount is the amount to bid on the new item.
+	 */
+	public void bid(final Item item, final double amount)
+	{
+		if(amount > item.statistics.getHighest().getAmount())
+		{
+			Bid bid = new Bid(this.getId(), amount);
+			item.addBid(bid);
+		}
+	}
+
+	/**
+	 * This method will bid on an item using the QR code.
+	 * 
+	 * @param item  the item to be bidded on.
+	 * @param amount is the amount to bid on the new item.
+	 */
+	public void bidQR(final Item item, final double amount)
+	{
+		//TODO maybe implemented.
+	}
+}
