7434159807c5503cb3fc26441e79abd011f65ac7
HSilva4 <hsilva@uw.edu>
2015-06-11 13:30:30 -0700

    Conner added bidtest

--- NewSilentAuction/src/backend/BidTest.java
+++ NewSilentAuction/src/backend/BidTest.java
@@ -2,63 +2,77 @@ package backend;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @author Conner Martin
+ *
+ */
 public class BidTest
 {
-  
-  @Test
-  public void testBid()
-  {
-    fail("Not yet implemented"); // TODO
-  }
-  
-  @Test
-  public void testGetAmount()
-  {
-    fail("Not yet implemented"); // TODO
-  }
-  
-  @Test
-  public void testGetBidder()
-  {
-    fail("Not yet implemented"); // TODO
-  }
-  
-  @Test
-  public void testContributionIntDouble()
-  {
-    fail("Not yet implemented"); // TODO
-  }
-  
-  @Test
-  public void testContributionIntDoubleString()
-  {
-    fail("Not yet implemented"); // TODO
-  }
-  
-  @Test
-  public void testGetDescription()
-  {
-    fail("Not yet implemented"); // TODO
-  }
-  
-  @Test
-  public void testGetUser()
-  {
-    fail("Not yet implemented"); // TODO
-  }
-  
-  @Test
-  public void testGetTimeStamp()
-  {
-    fail("Not yet implemented"); // TODO
-  }
-  
-  @Test
-  public void testGetValue()
-  {
-    fail("Not yet implemented"); // TODO
-  }
-  
+	
+	Bid testbid;
+	
+	@Before
+	public void before()
+	{
+		testbid = new Bid(0621, 15.69);
+	}
+
+	@Test
+	public void testBid()
+	{
+		assertNotNull(testbid);
+	}
+
+	@Test
+	public void testGetAmount()
+	{
+		double amount = testbid.getAmount();
+		assertEquals(15.69, amount, 1);
+	}
+
+	@Test
+	public void testGetBidder()
+	{
+		double bidder = testbid.getBidder();
+		assertEquals(0621, bidder, 1);
+	}
+
+	@Test
+	public void testGetID()
+	{
+		int id = testbid.getId();
+		assertEquals(0621, id, 1);
+	}
+
+	@Test
+	public void testGetDescription()
+	{
+		String description = testbid.getDescription();
+		assertEquals("None provided", description);
+	}
+
+	@Test
+	public void testGetUser()
+	{
+		int user = testbid.getUser();
+		assertEquals(0621, user, 1);
+	}
+
+	@Test
+	public void testGetTimeStamp()
+	{
+		double timestamp = testbid.getTimeStamp();
+		assertNotNull(timestamp);
+	}
+
+	@Test
+	public void testGetValue()
+	{
+		double value = testbid.getValue();
+		assertEquals(15.69, value, 1);
+	}
+
 }
